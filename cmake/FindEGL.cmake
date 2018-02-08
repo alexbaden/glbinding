8d729b223de176d20fd26ee5036b72cb979a9107
Alex Baden <alex.baden@mapd.com>
2018-02-07 16:41:46 -0800

    Find nvidia drivers libEGL without hardcoding path

--- cmake/FindEGL.cmake
+++ cmake/FindEGL.cmake
@@ -4,8 +4,40 @@
 # EGL_LIBRARY
 # EGL_FOUND
 
-find_path(EGL_INCLUDE_DIR NAMES EGL/egl.h PATHS ${CMAKE_SOURCE_DIR}/source/glbinding/include/egl /usr/include PATH_SUFFIXES nvidia nvidia-375 nvidia-367 nvidia-365 nvidia-361 nvidia-381 nvidia-384 aarch64-linux-gnu)
-find_library(EGL_LIBRARY NAMES egl EGL libEGL PATHS /usr/lib64 /usr/lib PATH_SUFFIXES nvidia nvidia-375 nvidia-367 nvidia-365 nvidia-361 nvidia-381 nvidia-384 aarch64-linux-gnu NO_DEFAULT_PATH)
+set(_EGL_INCLUDE_PREFIXES 
+    /usr/include
+    /usr/local/include
+    ${CMAKE_SOURCE_DIR}/source/glbinding/include/egl
+    /libglvnd/include
+)
+
+set(_EGL_LIBRARY_PREFIXES 
+    /usr/lib64
+    /usr/lib
+    /usr/local/lib
+)
+
+function(GetNvidiaDriverPathSuffix resultVar)
+    set(result)
+    foreach(ITR ${ARGN})
+        file(GLOB folders ${ITR}/*)
+        foreach(folder_path ${folders})
+            get_filename_component(folder ${folder_path} NAME)
+            if(folder MATCHES "^nvidia-[^-]*$")
+                list(APPEND result ${folder})
+            endif()
+        endforeach()
+    endforeach()
+    set(${resultVar} ${result} PARENT_SCOPE)
+endfunction()
+
+GetNvidiaDriverPathSuffix(NVIDIA_FOLDER_PATH_SUFFIX ${_EGL_LIBRARY_PREFIXES})
+if(NVIDIA_FOLDER_PATH_SUFFIX)
+    message(STATUS "Using nvidia drivers directory ${NVIDIA_FOLDER_PATH_SUFFIX}")
+endif()
+
+find_path(EGL_INCLUDE_DIR NAMES EGL/egl.h PATHS ${_EGL_INCLUDE_PREFIXES} PATH_SUFFIXES ${NVIDIA_FOLDER_PATH_SUFFIX} aarch64-linux-gnu)
+find_library(EGL_LIBRARY NAMES egl EGL libEGL PATHS ${_EGL_LIBRARY_PREFIXES} PATH_SUFFIXES ${NVIDIA_FOLDER_PATH_SUFFIX} aarch64-linux-gnu NO_DEFAULT_PATH)
 
 set(EGL_INCLUDE_DIRS ${EGL_INCLUDE_DIR})
 set(EGL_LIBRARIES ${EGL_LIBRARY})
